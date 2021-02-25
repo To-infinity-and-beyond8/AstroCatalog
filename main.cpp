d8d9b86c1c788ef801f5af6614b2287213891cf6
Ivanchik-sys <78605362+Ivanchik-sys@users.noreply.github.com>
2021-02-25 22:41:38 +0300

    Create main.cpp
    First commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class astroobjects
+{
+protected:
+    string name;
+    string age;
+    string distant;
+
+public:
+    void ShowName()
+    {
+        cout << name;
+    }
+
+    void ShowAge()
+    {
+        cout << age;
+    }
+
+    void ShoDistant()
+    {
+        cout << distant;
+    }
+
+    virtual ShowFacts() = 0;
+
+    virtual ShowDescription() = 0;
+};
+
+int main()
+{
+
+    return 0;
+}
